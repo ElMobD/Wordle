a63bb4bd7008a8c9416df95f5cd698d4b81889de
JoyBoy <106823243+ElMobD@users.noreply.github.com>
2026-03-07 22:43:07 +0100

    Chronomètre actif jusqu'à la fin du round puis disparition

--- backend/src/main/java/com/wordle/backend/websocket/response/WebSocketResponseFactory.java
+++ backend/src/main/java/com/wordle/backend/websocket/response/WebSocketResponseFactory.java
@@ -15,7 +15,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
